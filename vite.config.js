8763a945cac922bc0827df71bc0b35b143fce82a
Uhliber <olvrcp@gmail.com>
2023-10-12 02:00:18 +0800

    fix base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-swappable-widget/',
+  base: '/vue-swappable-widgets/',
   plugins: [
     vue(),
   ],
